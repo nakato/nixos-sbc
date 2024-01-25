accbd5438f069525b054c42416ffc50b8e039dcd
Sachi King <nakato@nakato.io>
2024-01-25 21:22:13 +1100

    Add support for bootstrap with BTRFS subvolume layout
    This is the format that is destened to be the default.  Subvolumes are
    useful, but they do have a drawback, you can't reflink between /nix and
    /boot without mounting the top-most subvolume and operating on it.
    
    Overall, having subvolumes provides a lot of flexibility for what is a
    small drawback.
    
    One such example of the flexibility is that you can quickly convert to
    using impermanence by including it, switching `/` to a tmpfs, mounting
    the top-most btrfs subvol, creating /@persistance, including it in your
    fileSystem layout, and rebooting, at which point you can nuke /@.
    
    Normally, I'd call /@, /@root, but some people want to put the root user
    on a subvol at /@root, so I'll use the /@ style for the `/` subvol here.

--- modules/sbc/bootstrap/default.nix
+++ modules/sbc/bootstrap/default.nix
@@ -27,7 +27,7 @@ in
     };
 
     rootFilesystem = mkOption {
-      type = types.enum [ "btrfs" "ext4" ];
+      type = types.enum [ "btrfs-subvol" "btrfs" "ext4" ];
       default = "ext4";
       description = mdDoc ''
         Format for root filesystem.
@@ -79,12 +79,45 @@ in
         compressImage = false;
         volumeLabel = "root";
         uuid = "18db6211-ac36-42c1-a22f-5e15e1486e0d";
-        populateImageCommands = ''
+        populateImageCommands =
+        let
+          ramify = ''
+            touch ./files/NIXOS_RAMIFY
+          '';
+        in ''
           mkdir ./files/boot
           ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
-        '';
+        '' + (if cfg.rootFilesystem == "btrfs-subvol" then ramify else "");
       };
-    in if cfg.rootFilesystem == "btrfs" then rootfsBtrfsImage else rootfsExt4Image;
+    in if (builtins.elem cfg.rootFilesystem [ "btrfs" "btrfs-subvol" ]) then rootfsBtrfsImage else rootfsExt4Image;
+
+    # postResumeCommands is right before mount happens, and after a bunch of helper functions are defined.
+    boot.initrd.postResumeCommands = let
+      # Root device should become /nix so users can go directly to impermanence.
+      # But that's not supported yet with the whole /nix-path-registration thing
+      rootDevice = (builtins.head (builtins.filter (fs: fs.mountPoint == "/") config.system.build.fileSystems)).device;
+    in lib.mkIf (cfg.rootFilesystem == "btrfs-subvol") ''
+      mkdir -p $targetRoot
+      ramifyDevice=${rootDevice}
+      mount -t btrfs -o compress=zstd $ramifyDevice $targetRoot
+      if [ -f $targetRoot/NIXOS_RAMIFY -a ! -d $targetRoot/@ ]; then
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume create $targetRoot/@nix
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume create $targetRoot/@boot
+        ${pkgs.btrfs-progs}/bin/btrfs subvolume create $targetRoot/@
+
+        cp -a --reflink $targetRoot/nix/* $targetRoot/@nix/
+        rm -rf $targetRoot/nix
+
+        cp -a --reflink $targetRoot/boot/* $targetRoot/@boot/
+        rm -rf $targetRoot/boot
+
+        find $targetRoot -mindepth 1 -maxdepth 1 -not -path "$targetRoot/@*" -exec cp -a --reflink {} $targetRoot/@ \;
+        find $targetRoot -mindepth 1 -maxdepth 1 -not -path "$targetRoot/@*" -exec rm -rf {} \;
+
+        rm $targetRoot/@/NIXOS_RAMIFY
+      fi
+      umount $targetRoot
+    '';
 
     boot.postBootCommands =
     let
--- modules/sbc/filesystem.nix
+++ modules/sbc/filesystem.nix
@@ -8,13 +8,13 @@ in
 {
   options.sbc.filesystem = with lib; {
     useDefaultLayout = mkOption {
-      type = types.enum [ "btrfs" "ext4" false ];
+      type = types.enum [ "btrfs-subvol" "btrfs" "ext4" false ];
       description = mdDoc ''
         When a pre-built SD image is used, the filesystem layout will be
         known to us, so by enabling the use of the default layout you do
         not need to specify it yourself.
 
-        Options are false, "btrfs" or "ext4".
+        Options are false, "btrfs-subvol" "btrfs" or "ext4".
       '';
     };
   };
@@ -25,6 +25,25 @@ in
         then config.sbc.bootstrap.rootFilesystem
         else false);
     }
+    (lib.mkIf (config.sbc.enable && cfg.useDefaultLayout == "btrfs-subvol") {
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-uuid/18db6211-ac36-42c1-a22f-5e15e1486e0d";
+          fsType = "btrfs";
+          options = [ "compress=zstd" "subvol=/@" ];
+        };
+        "/boot" = {
+          device = "/dev/disk/by-uuid/18db6211-ac36-42c1-a22f-5e15e1486e0d";
+          fsType = "btrfs";
+          options = [ "compress=zstd" "subvol=/@boot" ];
+        };
+        "/nix" = {
+          device = "/dev/disk/by-uuid/18db6211-ac36-42c1-a22f-5e15e1486e0d";
+          fsType = "btrfs";
+          options = [ "compress=zstd" "subvol=/@nix" ];
+        };
+      };
+    })
     (lib.mkIf (config.sbc.enable && cfg.useDefaultLayout == "btrfs") {
       fileSystems = {
         "/" = {
