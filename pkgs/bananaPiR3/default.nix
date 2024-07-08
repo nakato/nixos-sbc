784c535ae033339a515beb2a8aa34dde4f4772d1
Sachi King <nakato@nakato.io>
2024-07-08 16:14:20 +1000

    BPiR3: Minimal-ish kernel for CI builds
    I can't have my device doing 8 hour kernel builds on repeat.  This takes
    an hour.  Want additional moduels?  Add them and make a PR.

--- pkgs/bananaPiR3/default.nix
+++ pkgs/bananaPiR3/default.nix
@@ -140,5 +140,209 @@
       REGULATOR_MT6380 = yes;
     };
   });
+  linuxPackages_6_9_bananaPiR3_minimal = linuxKernel.packagesFor (linuxPackages_6_9_bananaPiR3.kernel.override {
+    autoModules = false;
+
+    structuredExtraConfig = with lib.kernel;
+      linuxPackages_6_9_bananaPiR3.kernel.structuredExtraConfig
+      // {
+        ARCH_ACTIONS = no;
+        ARCH_SUNXI = no;
+        ARCH_ALPINE = no;
+        ARCH_APPLE = no;
+        ARCH_BCM = no;
+        ARCH_BCM2835 = no;
+        ARCH_BCM_IPROC = no;
+        ARCH_BCMBCA = no;
+        ARCH_BRCMSTB = no;
+        ARCH_BERLIN = no;
+        ARCH_EXYNOS = no;
+        ARCH_SPARX5 = no;
+        ARCH_K3 = no;
+        ARCH_LG1K = no;
+        ARCH_HISI = no;
+        ARCH_KEEMBAY = no;
+        ARCH_MESON = no;
+        ARCH_MVEBU = no;
+        ARCH_NXP = no;
+        ARCH_LAYERSCAPE = no;
+        ARCH_MXC = no;
+        ARCH_S32 = no;
+        ARCH_MA35 = no;
+        ARCH_NPCM = no;
+        ARCH_QCOM = no;
+        ARCH_REALTEK = no;
+        ARCH_RENESAS = no;
+        ARCH_ROCKCHIP = no;
+        ARCH_SEATTLE = no;
+        ARCH_INTEL_SOCFPGA = no;
+        ARCH_STM32 = no;
+        ARCH_SYNQUACER = no;
+        ARCH_TEGRA = no;
+        ARCH_TESLA_FSD = no;
+        ARCH_SPRD = no;
+        ARCH_THUNDER = no;
+        ARCH_THUNDER2 = no;
+        ARCH_UNIPHIER = no;
+        ARCH_VEXPRESS = no;
+        ARCH_VISCONTI = no;
+        ARCH_XGENE = no;
+        ARCH_ZYNQMP = no;
+
+        # If both ACPI and DT supported, one is picked at boot.
+        # This device is DT based, no need for any ACPI stuff.
+        ACPI = no;
+
+        CAN = no;
+        NFC = no;
+        NET_9P = no;
+        XEN = lib.mkForce no;
+        XEN_DOM0 = lib.mkForce no;
+
+        VIRT_DRIVERS = lib.mkForce no;
+        STAGING_MEDIA = no;
+        CHROME_PLATFORMS = lib.mkForce no;
+        COMMON_CLK_RK808 = no;
+        COMMON_CLK_SCMI = no;
+        COMMON_CLK_SCPI = no;
+        COMMON_CLK_CS2000_CP = no;
+        COMMON_CLK_S2MPS11 = no;
+        COMMON_CLK_XGENE = no;
+        COMMON_CLK_PWM = no;
+        COMMON_CLK_RS9_PCIE = no;
+        COMMON_CLK_VC3 = no;
+        COMMON_CLK_VC5 = no;
+        COMMON_CLK_BD718XX = no;
+
+        SOUNDWIRE = no;
+
+        ARM_SCMI_PERF_DOMAIN = no;
+        ARM_SCMI_POWER_DOMAIN = no;
+        ARM_SCPI_POWER_DOMAIN = no;
+
+        IIO = no;
+
+        FPGA = no;
+
+        ISO9660_FS = lib.mkForce no;
+        UDF_FS = lib.mkForce no;
+
+        NETWORK_FILESYSTEMS = no;
+
+        GOOGLE_FIRMWARE = no;
+        GOOGLE_CBMEM = no;
+        GOOGLE_COREBOOT_TABLE = no;
+
+        EFI_ESRT = no;
+        EFI_VARS_PSTORE = no;
+        EFI_PARAMS_FROM_FDT = no;
+        EFI_RUNTIME_WRAPPERS = no;
+        EFI_GENERIC_STUB = no;
+        EFI_ARMSTUB_DTB_LOADER = no;
+        EFI_CAPSULE_LOADER = no;
+        EFI_EARLYCON = no;
+
+        HID_A4TECH = no;
+        HID_APPLE = no;
+        HID_BELKIN = no;
+        HID_CHERRY = no;
+        HID_CHICONY = no;
+        HID_CYPRESS = no;
+        HID_EZKEY = no;
+        HID_ITE = no;
+        HID_KINSINGTON = no;
+        HID_LOGITECH = no;
+        LOGIRUMBLEPAD2_FF = lib.mkForce no;
+        LOGIG940_FF = lib.mkForce no;
+        HID_REDRAGON = no;
+        HID_MICROSOFT = no;
+        HID_MONTEREY = no;
+        HID_MULTITOUCH = no;
+        I2C_HID = no;
+
+        HW_RANDOM_BCM2835 = no;
+        HW_RANDOM_IPROC_RNG200 = no;
+        HW_RANDOM_OMAP = no;
+        HW_RANDOM_VIRTIO = no;
+        HW_RANDOM_HISI = no;
+        HW_RANDOM_HISTB = no;
+        HW_RANDOM_XGENE = no;
+        HW_RANDOM_STM32 = no;
+        HW_RANDOM_MESON = no;
+        HW_RANDOM_CAVIUM = no;
+        HW_RANDOM_EXYNOS = no;
+        HW_RANDOM_OPTEE = no;
+        HW_RANDOM_NPCM = no;
+
+        LIRC = lib.mkForce no;
+        CEC_CORE = no;
+        MEDIA_CEC_RC = lib.mkForce no;
+        MEDIA_CEC_SUPPORT = no;
+        MEDIA_SUPPORT = no;
+        MEDIA_USB_SUPPORT = lib.mkForce no;
+        VIDEO_MEDIATEK_JPEG = no;
+        VIDEO_MEDIATEK_VCODEC_SCP = no;
+        VIDEO_MEDIATEK_VCODEC = no;
+        VIDEO_IMX_MIPI_CSIS = no;
+        VIDEO_IMX8_ISI = no;
+        VIDEO_IMX8_ISI_M2M = no;
+        VIDEO_IR_I2C = no;
+        VIDEO_CAMERA_SENSOR = no;
+
+        WIREGUARD = module;
+        IPVLAN = module;
+        VXLAN = module;
+        GENEVE = module;
+
+        NET_VENDOR_MELLANOX = no;
+
+        NF_TABLES = yes;
+        NETFILTER_NETLINK_HOOK = module;
+        NETFILTER_NETLINK_ACCT = module;
+        NETFILTER_NETLINK_QUEUE = module;
+        NETFILTER_NETLINK_LOG = module;
+        NETFILTER_NETLINK_OSF = module;
+        NF_CONNTRACK_MARK = yes;
+        NF_CONNTRACK_LABELS = yes;
+        NF_CONNTRACK_AMANDA = module;
+        NF_CONNTRACK_FTP = module;
+        NF_CONNTRACK_H323 = module;
+        NF_CONNTRACK_IRC = module;
+        NF_CONNTRACK_NETBIOS_NS = module;
+        NF_CONNTRACK_SNMP = module;
+        NF_CONNTRACK_PPTP = module;
+        NF_CONNTRACK_SANE = module;
+        NF_CONNTRACK_SIP = module;
+        NF_CONNTRACK_TFTP = module;
+        NF_CT_NETLINK = module;
+        NF_CT_NETLINK_TIMEOUT = module;
+        NFT_NUMGEN = module;
+        NFT_CT = module;
+        NFT_CONNLIMIT = module;
+        NFT_LOG = module;
+        NFT_LIMIT = module;
+        NFT_MASQ = module;
+        NFT_REDIR = module;
+        NFT_NAT = module;
+        NFT_TUNNEL = module;
+        NFT_QUOTA = module;
+        NFT_REJECT = module;
+        NFT_COMPAT = module;
+        NFT_HASH = module;
+        NFT_SOCKET = module;
+        NFT_OSF = module;
+        NFT_TPROXY = module;
+        NFT_SYNPROXY = module;
+        NF_DUP_NETDEV = module;
+        NFT_DUP_NETDEV = module;
+        NFT_FWD_NETDEV = module;
+        NF_FLOW_TABLE = module;
+
+        CRYPTO_POLYVAL_ARM64_CE = module;
+        CRYPTO_DEV_SAFEXCEL = module;
+        SFP = module;
+        MDIO_I2C = module;
+      };
+  });
   linuxPackages_latest_bananaPiR3 = linuxPackages_6_9_bananaPiR3;
 }
